42529a17dc8b68daf67930dec6f9775c41aad7de
Sean Powell <sean@the-powells.org>
2024-02-06 01:30:00 -0600

    fix vagrant win connection issues

--- manage_vagrant_boxes.ps1
+++ manage_vagrant_boxes.ps1
@@ -1,5 +1,11 @@
+# "V:\Repos\Github\windows-packed-vmware-boxes\images\box\WS2019_1223_vmware.box"
+
+Set-Location -Path $PSScriptRoot
 
 vagrant box remove ws2019
-vagrant box add ws2019 ../windows-packed-vmware-boxes/images/box/ws2019_vmware.box
+vagrant box add ws2019 ../windows-packed-vmware-boxes/images/box/ws2019_1223_vmware.box
+
+vagrant box remove W10
+vagrant box add w10 ../windows-packed-vmware-boxes/images/box/W10e_22h2_020324_vmware.box
 
 exit
